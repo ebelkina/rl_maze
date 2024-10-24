d401c60a778fc6e42d7140f9951399510bcafad0
Elena Belkina <e.belkina@hotmail.com>
2024-10-24 17:17:09 +0200

    maze makes deep copy, sub_goals added to results, visualized 3 plots

--- maze_v18_main.py
+++ maze_v18_main.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from maze_v18 import MazeEnv
 from scipy.stats import ttest_rel
+import copy
 
 # Register the custom environment
 gym.register(
@@ -13,19 +14,16 @@ gym.register(
     kwargs={'maze': None}
 )
 
-def visualize_results(id, path, results):
-    # Convert results to numpy arrays for easy computation
-    for key in results:  # keys: ('q-learning', 0), ('q-learning', 0.1), ('sarsa', 0), ('sarsa', 0.1)
-        results[key] = np.array(results[key])
-        # print(key)
-        # print(results[key])
-
-    # Calculate average and standard deviation of rewards
+def visualize_results(id, path, results, sub_goal):
+    # Calculate average and standard deviation of rewards for each algorithm and epsilon
     mean_total_rewards = {}
     std_total_rewards = {}
-    for key in results:  # keys: ('q-learning', 0), ('q-learning', 0.1), ('sarsa', 0), ('sarsa', 0.1)
-        mean_total_rewards[key] = np.mean(results[key], axis=0)
-        std_total_rewards[key] = np.std(results[key], axis=0)
+
+    for (sg, alg, eps) in results:
+        if sg == sub_goal:
+            rewards_array = np.array(results[(sg, alg, eps)])
+            mean_total_rewards[(alg, eps)] = np.mean(rewards_array, axis=0)
+            std_total_rewards[(alg, eps)] = np.std(rewards_array, axis=0)
 
     # Paired t-test between Q-learning and SARSA with epsilon = 0.1
     # q_learning_rewards = mean_rewards[('q-learning', 0.1)]
@@ -55,9 +53,10 @@ def visualize_results(id, path, results):
     #             num_not_reach_max[alg] += 1
 
     # Save the results to CSV
-    df_results = pd.DataFrame(
-        {f"{alg}_{eps}": np.mean(results[(alg, eps)], axis=0) for alg in algorithms for eps in epsilons})
-    df_results.to_csv('results.csv', index=False)
+    df_results = pd.DataFrame({
+        f"{alg}_{eps}": mean_total_rewards[(alg, eps)] for (alg, eps) in mean_total_rewards
+    })
+    df_results.to_csv(f'{path}_subgoal_{sub_goal}_results.csv', index=False)
 
     # Print maximum reward comparison stats
     # print("First episode where Q-learning reached max reward:", first_reach_max['q-learning'])
@@ -65,20 +64,22 @@ def visualize_results(id, path, results):
     # print("Number of times Q-learning did not reach max reward:", num_not_reach_max['q-learning'])
     # print("Number of times SARSA did not reach max reward:", num_not_reach_max['sarsa'])
 
-    # Plot the results
+    # Plot the results for the sub-goal
     plt.figure()
-    for eps in epsilons:
-        for alg in algorithms:
-            label = f"{alg}_{eps}"
-            plt.plot(range(episodes), mean_total_rewards[(alg, eps)], label=label)
-            plt.fill_between(range(episodes),
-                             mean_total_rewards[(alg, eps)] - std_total_rewards[(alg, eps)],
-                             mean_total_rewards[(alg, eps)] + std_total_rewards[(alg, eps)],
-                             alpha=0.2)
+    for (alg, eps) in mean_total_rewards:
+        label = f"{alg} (ε={eps})"
+        plt.plot(range(len(mean_total_rewards[(alg, eps)])), mean_total_rewards[(alg, eps)], label=label)
+        plt.fill_between(
+            range(len(mean_total_rewards[(alg, eps)])),
+            mean_total_rewards[(alg, eps)] - std_total_rewards[(alg, eps)],
+            mean_total_rewards[(alg, eps)] + std_total_rewards[(alg, eps)],
+            alpha=0.2
+        )
 
+    plt.title(f'Sub-goal at Position {sub_goal}')
     plt.xlabel('Episodes')
     plt.ylabel('Average Total Rewards')
-    plt.ylim(-200, 200)
+    plt.ylim(-300, 100)
     plt.legend(loc='lower right')
     plt.grid(which='major', linestyle='--', linewidth=0.75)
     plt.show()
@@ -109,7 +110,7 @@ sub_goals_map = {
 # Set a sub-goal in the maze
 def place_sub_goal(maze, sub_goal_pos):
     row, col = sub_goal_pos
-    new_maze = maze.copy()
+    new_maze = copy.deepcopy(maze)
     # Check if sub-goal position is empty (not wall, start or end-goal)
     if new_maze[row][col] == 0:
         new_maze[row][col] = 'G'
@@ -118,40 +119,43 @@ def place_sub_goal(maze, sub_goal_pos):
         print("Sub-goal cannot be places")
 
 ### Parameters
-episodes = 10
+episodes = 200
 
 sleep_sec = 0
 # sleep_sec = 0.05
 # sleep_sec = 0.5
 
-num_experiments = 10 # equal to seed
+num_experiments = 50 # equal to seed
 show = False
-show = True
+# show = True
 
 # show = False
 # algorithms = ["q-learning"]
-algorithms = ["sarsa"]
-# algorithms = ["q-learning", "sarsa"]
+# algorithms = ["sarsa"]
+algorithms = ["q-learning", "sarsa"]
 
-epsilons = [0.01]
+epsilons = [0.1]
 # epsilons = [0, 0.001, 0.01, 0.1]
 
 reduce_epsilon = False
 reduce_epsilon = True # TODO
 
 alpha = 0.05
-epsilon = 0.1
-# Initialize a dictionary to store the results for each algorithm
-# keys: ('q-learning', 0), ('q-learning', 0.1), ('sarsa', 0), ('sarsa', 0.1)
-results = {(alg, eps): [] for alg in algorithms for eps in epsilons}
+# epsilon = 0.1
 
 # Maximum possible reward for the maze
 max_possible_reward = 137  # TODO check
 
 sub_goals = [(5,3), (7,3), (1,4)]
-sub_goals = [(5,3)]
+# sub_goals = [(5,3)]
 # for sub_goal, _ in sub_goals_map.items():
 #     maze_with_sub_goal = place_sub_goal(maze_np, sub_goal)
+
+# Initialize a dictionary to store the results for each algorithm
+# keys: ('q-learning', 0), ('q-learning', 0.1), ('sarsa', 0), ('sarsa', 0.1)
+results = {(sub_goal, alg, eps): [] for sub_goal in sub_goals for alg in algorithms for eps in epsilons}
+
+
 
 id = 1
 path = '.\\results'
@@ -167,17 +171,19 @@ for sub_goal in sub_goals:
                 rewards = []
                 # env = gym.make('Maze_v18', maze=maze_with_sub_goal, sub_goal=sub_goal, epsilon=epsilon, algorithm=alg,
                 #                experiment=experiment, show=show, reduce_epsilon=reduce_epsilon)
-                env = gym.make('Maze_v18', maze=maze_with_sub_goal, epsilon=epsilon, algorithm=alg,
+                env = gym.make('Maze_v18', maze=maze_with_sub_goal, epsilon=eps, algorithm=alg,
                                experiment=experiment, show=show, reduce_epsilon=reduce_epsilon, alpha=alpha)
                 env.reset()
                 env.render()
                 rewards, path_lengths, q_table_1, q_table_2 = env.train(episodes, sleep_sec)
-                results[(alg, eps)].append(rewards)
+                results[(sub_goal, alg, eps)].append(rewards)
                 # csv
                 learned_path, learned_path_reward = env.show_learned_path()
                 # print("learned_path", learned_path)
                 print("learned_path_reward", learned_path_reward)
 
-visualize_results(id, path, results)
+# Visualize the results for each sub-goal
+for sub_goal in sub_goals:
+    visualize_results(id, path, results, sub_goal)
 
 
